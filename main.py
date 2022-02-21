67b19fbe4d1da079799d027962327909a13c1a40
Andrei Ciurba <andrei.ciurba@microfocus.com>
2022-02-21 21:04:46 +0200

    think i did it needs some error fixing and exception handling but seems okay

--- main.py
+++ main.py
@@ -1,27 +1,67 @@
 # Import required modules
-from lxml import html
+from lxml import html, etree
 import requests
-from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
-import pandas as pd
+import email
+# import pandas as pd
 from openpyxl import load_workbook
 
+from bs4 import BeautifulSoup
+from html_table_extractor.extractor import Extractor
 
-def get_column_values():
-    file_name = u'C:/Users/ACiurba/Desktop/python_web_scraper/testCSV.xlsx'
-    wb = load_workbook(file_name)  # Work Book
-    ws = wb.get_sheet_by_name('Sheet1')  # Work Sheet
-    column = ws['B']  # Column
-    
-    column_list = [str(column[x].value) for x in range(1,len(column))]
-    return column_list
 
-column_list = get_column_values()
+    #
+    # #setup web
+    # #add loop through all files
+    #
+    # #for file in all files:
+    file_name = u'C:/Users/ACiurba/Desktop/similarwebdownloads/test.html'
+    count = 2
+    html_report_part1 = open(file_name,'r', errors="ignore")
+    soup = BeautifulSoup(html_report_part1, 'html.parser')
+    html_string = soup.prettify()
+    start = 'wa-overview__engagement-value">'
+    end = '</p>'
 
-driver = webdriver.Firefox(executable_path=r'C:/Users/ACiurba/Downloads/geckodriver-v0.30.0-win64/geckodriver.exe')
-driver.get("https://www.similarweb.com/")
-for item in column_list:
-    inputElement = driver.find_element_by_id("search")
-    inputElement.send_keys(item)
-    inputElement.send_keys(Keys.ENTER)
-    driver.navigate().back();
+    print((html_string.split(start))[1].split(end)[0])
+    total_visits_location = 'F' + str(count)
+    ws[total_visits_location] = "asda"
+
+                # for table in soup.body.find_all("p", recursive=False):
+                #     extractor = Extractor(table)
+                #     extractor.parse()
+                #     print(extractor.return_list())
+
+
+    #
+    #
+    # distributor = soup.find("p", {"class": '3D"wa-overview__title"'}).get_text().split('<=')[0]
+    # print(soup.find("tspan", {"class": '3D"wa-traffic-sources__channels-data-label"'}).get_text())
+
+    #
+    #
+    # url = soup.find("p", {"class": '3D"wa-overview__title"'}).get_text().split('<=')[0]
+    # print(soup.find("tspan", {"class": '3D"wa-traffic-sources__channels-data-label"'}).get_text())
+
+    #
+    # country = soup.find("p", {"class": '3D"wa-overview__title"'}).get_text().split('<=')[0]
+    # print(soup.find("tspan", {"class": '3D"wa-traffic-sources__channels-data-label"'}).get_text())
+
+    #
+    #
+    #
+    # total_visits = soup.findAll("p", {"class": '3D"wa-overview__engagement-value"'})[0].get_text().split('<=')[0]
+    # print(soup.findAll("p", {"class": '3D"wa-overview__engagement-value"'})[0].get_text().split('<=')[0]
+
+
+
+
+    #
+    # wb.save(file_name_xcel)
+open_xcel()
+
+
+
+#
+# all fara search din tabel
+# alea 4 de sus
+# Organic vs. Paid
